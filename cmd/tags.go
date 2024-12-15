7de25e75d9322d9206bac9a9d8913d5a75925ecc
almond Heil <almond@almendra.dev>
2024-12-14 19:10:42 -0600

    Add tags.go because I do actually use that functionality

---
+++ cmd/tags.go
@@ -0,0 +1,25 @@
+/*
+Copyright © 2024 almond Heil <contact@almendra.dev>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// tagsCmd represents the tags command
+var tagsCmd = &cobra.Command{
+	Use:   "tags"
+	Short: "List tags across existing notes."
+	Aliases: []string{"t"},
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("tags called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(tagsCmd)
+}
